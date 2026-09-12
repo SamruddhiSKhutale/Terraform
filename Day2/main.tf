41f60d8693d96a99ad0bb587b5a47affa1e17130
SamruddhiSKhutale <samruddhikhutale1407@gmail.com>
2026-09-12 11:23:07 +0530

    Day2

---
+++ Day2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_vpc" "sam_vpc" {
+    cidr_block ="10.0.0.0/24"
+    tags ={
+        Name ="sam_vpc"
+    }
+}
+resource "aws_subnet" "sam_subnet" {
+    vpc_id = aws_vpc.sam_vpc.id
+    cidr_block = "10.0.0.0/28"
+    tags ={
+        Name ="sam_subnet"
+    }
+}
+
+resource "aws_security_group" "sam_sg" {
+    name = "sam_sg"
+    description = "Allow SSH and HTTP"
+    vpc_id = aws_vpc.sam_vpc.id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
